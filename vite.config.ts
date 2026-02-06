584a4178e1132c22acd2344d908070e0cd4bf29b
Madubha Amunugama <39756282+MadubhaTA@users.noreply.github.com>
2026-02-06 11:03:20 +0530

    Update vite.config.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
+  base: '/madubha-t-amunugama-portfolio/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
